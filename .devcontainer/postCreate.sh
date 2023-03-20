2b56fcc88f2b81907ff7695b62727bd2172bb182
Ian Edwards <isedwards@users.noreply.github.com>
2023-03-20 17:29:29 +0000

    Add opencdms-workshop

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -5,5 +5,11 @@ git clone https://github.com/opencdms/pyopencdms
 git clone https://github.com/opencdms/opencdms-app
 git clone https://github.com/opencdms/opencdms-api
 git clone https://github.com/opencdms/opencdms-test-data
+git clone https://github.com/opencdms/opencdms-workshop
 
 pip3 install -e /workspaces/pyopencdms
+# pip3 install -e /workspaces/opencdms-api
+pip3 install -e /workspaces/opencdms-workshop
+
+# Switch to workshop directory
+cd opencdms-workshop
