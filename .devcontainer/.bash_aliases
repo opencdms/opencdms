d0d5db805aebc0ce1b3459225736d83f8c0ce66e
Ian Edwards <isedwards@users.noreply.github.com>
2023-03-20 15:52:16 +0000

    Update postCreateCommand to use script

---
+++ .devcontainer/.bash_aliases
@@ -0,0 +1,9 @@
+alias ll='ls -l'
+alias la='ls -A'
+alias l='ls -CF'
+
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,7 @@
+cp /workspaces/opencdms/.devcontainer/.bash_aliases ~/
+source /workspaces/opencdms/.devcontainer/.bash_aliases
+
+git clone https://github.com/opencdms/pyopencdms
+git clone https://github.com/opencdms/opencdms-app
+git clone https://github.com/opencdms/opencdms-api
+git clone https://github.com/opencdms/opencdms-test-data
