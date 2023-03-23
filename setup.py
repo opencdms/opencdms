ef61cefdaa25575ca83724e6d8cf4b35e1ea2ea2
isedwards <ian@myacorn.com>
2023-03-23 11:26:38 +0000

    bumpversion config in setup.cfg

--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/opencdms/opencdms",
-    version="0.1.2",
+    version="0.1.1",
     zip_safe=False,
 )
