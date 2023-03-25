ba534d82d835827de566f886305c933585abb7fb
Ian Edwards <isedwards@users.noreply.github.com>
2023-03-25 12:47:22 +0000

    Disable pyopencdms install incase name conflict with opencdms cli

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -25,7 +25,7 @@ curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo 
 && sudo apt update \
 && sudo apt install gh -y
 
-pip3 install -e /workspaces/pyopencdms
+## pip3 install -e /workspaces/pyopencdms
 ## pip3 install -e /workspaces/opencdms-api
 ## pip3 install -e /workspaces/opencdms-workshop
 
