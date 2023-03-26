e3ea868422720be37ef169f4807cabbc440435f4
Ian Edwards <isedwards@users.noreply.github.com>
2023-03-26 15:16:19 +0000

    WIP codespace cli and ansible

---
+++ opencdms/cli/ansible/_run.py
@@ -0,0 +1,27 @@
+# TODO: Running ansible playbooks from Python is not working yet
+"""
+sudo apt-get update
+sudo apt-get install ansible
+pip install ansible-runner
+"""
+import os
+from ansible_runner import run
+
+# Set the path to the directory containing the playbook
+playbook_path = os.path.abspath('install-gh-cli.yml')
+inventory='localhost,'
+options={'inventory': inventory, 'extravars': {'ansible_python_interpreter': '/usr/bin/python3'}, 'debug': True}
+run(playbook=playbook_path, **options)
+
+'''
+# Create the runner object
+runner = ansible_runner.run(
+    private_data_dir=".",
+    playbook=playbook,
+    inventory=inventory,
+)
+# Print the results
+print(runner.status)
+print(runner.stdout)
+print(runner.stderr)
+'''
