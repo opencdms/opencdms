781657e30d2816af341e3527e92f746a0b262901
Ian Edwards <isedwards@users.noreply.github.com>
2023-03-26 17:15:23 +0000

    Add opencdms-test-databases as dev requirement

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -17,14 +17,6 @@ git clone https://github.com/opencdms/opencdms-test-data
 git clone https://github.com/opencdms/opencdms-test-databases
 git clone https://github.com/opencdms/opencdms-workshop
 
-# Setup gh cli
-type -p curl >/dev/null || sudo apt install curl -y
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-&& sudo apt update \
-&& sudo apt install gh -y
-
 # For development we want editable versions of the installed packages
 # even if `opencdms install` is ran later
 pip3 install -e /workspaces/pygeoapi
