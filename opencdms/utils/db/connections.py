5a9ad8d4fef4a09d3b24ea4ae22f833976e260b3
Ian Edwards <isedwards@users.noreply.github.com>
2023-04-02 19:35:04 +0100

    Add utils.db.connections:get_connection_strings

---
+++ opencdms/utils/db/connections.py
@@ -0,0 +1,41 @@
+"""
+This module contains a function for working with database connection strings for known databases.
+
+"""
+
+import configparser
+import os
+from typing import Optional, Union, Dict
+
+
+def get_connection_strings(database_name: Optional[str] = None) -> Union[str, Dict[str, str]]:
+    """Return dictionary value for given database_name if provided, else return full dictionary.
+
+    Args:
+        database_name: Optional name of the database to retrieve the connection string for.
+
+    Returns:
+        If `database_name` is provided, return the connection string for that database as a string.
+        If `database_name` is not provided, return the entire dictionary of connection strings.
+
+        If the `database_name` is not found in the configuration file, return a string indicating that.
+
+    """
+    config = configparser.ConfigParser()
+    config.read(base_path('db/alembic/alembic.ini'))
+    connections = {}
+    for section in config.sections():
+        connection = config[section]['sqlalchemy.url']
+        if connection.startswith('${') and connection.endswith('}'):
+            connection = os.environ.get(connection[2:-1], '')
+        if section == 'alembic':
+            connections['Default'] = connection
+        else:
+            connections[section] = connection
+    if database_name:
+        if database_name in connections:
+            return connections[database_name]
+        else:
+            return f'{database_name} not found'
+    else:
+        return connections
