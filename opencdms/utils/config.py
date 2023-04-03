2b007c8873eace941a2c117702815a650dd9c157
Ian Edwards <isedwards@users.noreply.github.com>
2023-04-03 10:31:43 +0100

    Correct legacy connection details

--- opencdms/utils/config.py
+++ opencdms/utils/config.py
@@ -12,7 +12,7 @@ class OpenCDMSConfig:
     CDM_DB_PORT = os.getenv("CDM_DB_PORT", "35432")
     CDM_DB_USER = os.getenv("CDM_DB_USER", "postgres")
     CDM_DB_PASS = os.getenv("CDM_DB_PASSWORD", "password")
-    CDM_DB_NAME = os.getenv("CDM_DB_NAME", "postgres")
+    CDM_DB_NAME = os.getenv("CDM_DB_NAME", "opencdmsdb")
     CDM_DB_ENGINE = os.getenv("CDM_DB_ENGINE", "postgresql")
     CDM_DB_DRIVER = os.getenv("CDM_DB_DRIVER", "psycopg2")
 config = OpenCDMSConfig()
