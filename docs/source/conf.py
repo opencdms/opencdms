0674d7f34397522893a5b7474a80bf6bd49b0302
david-i-berry <dberry@wmo.int>
2023-07-21 15:54:03 +0200

    Updates to developer docs.
    Issues with CSS and max width to be resolved.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -58,7 +58,7 @@ extensions = [
     'sphinxcontrib.openapi',
     'sphinx-jsonschema'
 ]
-
+panels_add_bootstrap_css = False
 openapi_default_renderer = 'httpdomain'
 
 # Add any paths that contain templates here, relative to this directory.
@@ -118,6 +118,10 @@ html_context = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_css_files = [
+    'css/custom.css',
+]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
